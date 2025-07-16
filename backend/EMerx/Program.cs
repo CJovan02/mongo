95d247abfb93336a73bac110fb1a2f045b581db8
Aleksa Perić <alex.peric1205@gmail.com>
2025-07-16 20:04:43 +0200

    Comments and gitignore

--- backend/EMerx/Program.cs
+++ backend/EMerx/Program.cs
@@ -1,3 +1,17 @@
+//User - name surname email password id
+//Product - id, price, name, category, image
+//Review - id, userid, productid, rating, ...
+//Order - id, quantity, productid, userid, address, ... 
+
+//result
+//error<list>
+//isokay
+//iserror
+//ierror
+//error - features/users/errors/
+//exception - constructor/create/mapping
+//automapper?
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
