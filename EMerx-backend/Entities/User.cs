9cc183e7c78af3ac074ed9ba5ab29fe87cfcc5c7
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-07-19 12:45:30 +0200

    backed up project

---
+++ EMerx-backend/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace EMerx.Entities;
+
+public class User : BaseEntity
+{
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required string Email { get; set; }
+    //temporarily will be hashed in prod
+    public required string Password { get; set; }
+    public Address? Address { get; set; }
+    public List<Order> Orders { get; set; } = new();
+    public List<Review> Reviews { get; set; } = new();
+}
--- EMerx-backend/Program.cs
+++ EMerx-backend/Program.cs
@@ -1,12 +1,22 @@
+//Product - id, price, name, category, image
+//Review - id, userid, productid, rating, ...
+//Order - id, quantity, productid, userid, address, ... 
+
+//result
+//error<list>
+//isokay
+//iserror
+//ierror
+//error - features/users/errors/
+//exception - constructor/create/mapping
+//automapper?
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +24,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
