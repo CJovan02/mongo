7f2800c578a9e2558bdf9ed84679b0a4a877f19b
Aleksa Perić <alex.peric1205@gmail.com>
2025-07-19 14:50:20 +0200

    Added result pattern

---
+++ EMerx-backend/Result/OrderErrors.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Bson;
+
+namespace EMerx_backend.Result;
+
+public static class OrderErrors
+{
+    public static Error NotFound(ObjectId id) => new(StatusCodes.NotFound, $"Order with id {id} not found.");
+}
---
+++ EMerx-backend/Result/UserErrors.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson;
+
+namespace EMerx_backend.Result;
+
+public static class UserErrors
+{
+    public static Error NotFound(ObjectId id) => new(StatusCodes.NotFound, $"User with id {id} not found.");
+    public static Error EmailOccupied(string email) => new(StatusCodes.Conflict, $"Email {email} is already taken.");
+}
